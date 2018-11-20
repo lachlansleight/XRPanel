5c5ed2e08919ee1140efbf66496f345a195357d3
Lachlan Sleight <lachlansleight@gmail.com>
2018-11-20 21:08:01 +1100

    Added GeometryFixer to allow various scales for controls

--- Assets/XRPanel/Scripts/XrpControl.cs
+++ Assets/XRPanel/Scripts/XrpControl.cs
@@ -71,5 +71,11 @@ namespace XRP
 		{
 			return Vector3.zero;
 		}
+
+		[ContextMenu("FixGeometry")]
+		public void FixGeometry()
+		{
+			GetComponentInChildren<XrpControlGeometry>().FixGeometry();
+		}
 	}
 }
